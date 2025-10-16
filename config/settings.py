2641d33cb68514225498b6df23f8c907e6c54bda
Nikita <nikkitos.pershin.2000@mail.ru>
2025-10-16 11:33:11 +0500

    updates and migrations

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    'rest_framework_simplejwt',
+    "rest_framework_simplejwt",
+    "django_filters",
     'users',
     'habit',
 ]
