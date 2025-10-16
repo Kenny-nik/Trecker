5d70d4d0f891213eb95f8cff965160f40e6e54f6
Nikita <nikkitos.pershin.2000@mail.ru>
2025-10-16 10:55:55 +0500

    update

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="email", help_text="Введите Email"
+    )
+    city = models.CharField(
+        max_length=20, verbose_name="city", help_text="Укажите город", **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars", verbose_name="Аватар", **NULLABLE
+    )
+    id_tg = models.CharField(
+        max_length=50,
+        verbose_name="id_tg",
+        help_text="Введите Chat-ID Telegram",
+        **NULLABLE
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
